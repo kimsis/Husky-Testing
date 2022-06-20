3198a451e359e91848fbda6073900d5fc026ee3d
dhristov <d.hristov@sportradar.com>
2022-06-20 17:37:11 +0200

    fix husky?

--- husky.config.js
+++ husky.config.js
@@ -1,9 +1,6 @@
-const runYarnLock = "yarn install --frozen-lockfile";
-
 module.exports = {
   hooks: {
-    "post-checkout": `if [[ $HUSKY_GIT_PARAMS =~ 1$ ]]; then ${runYarnLock}; fi`,
-    "post-merge": runYarnLock,
+    "post-merge": "yarn install --frozen-lockfile",
     "post-rebase": "yarn install",
     "pre-commit": "yarn lint-staged",
     "commit-msg": 'npx --no -- commitlint --edit "${1}"\n',
