a8e4a642e531deb0a1f03ff59baa5a2388cd3c15
dhristov <d.hristov@sportradar.com>
2022-07-01 15:55:12 +0200

    (fix) DSP-32: working?

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,34 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 100],
+    "subject-case": [2, "never", ["sentence-case"]],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-empty": [2, "never"],
+    "type-case": [2, "always", "lower-case"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "merge request",
+      ],
+    ],
+  },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^\(([a-z]+)\)?\s?([A-Z]+-\d+):\s?(.*)$/,
+      headerCorrespondence: ["type", "ticket", "subject"],
+    },
+  },
 };
