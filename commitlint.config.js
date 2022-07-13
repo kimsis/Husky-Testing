88198bda5436a9a1bbfdc9dd1132d6c96f0af0c4
dhristov <d.hristov@sportradar.com>
2022-07-13 10:41:45 +0200

    (fix) HSK-1: Testing fix.

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "header-max-length": [2, "always", 100],
     "subject-case": [2, "always", ["sentence-case", "snake-case"]],
     "subject-empty": [2, "never"],
-    "subject-full-stop": [2, "never", "."],
+    "subject-full-stop": [2, "always", "."],
     "type-empty": [2, "never"],
     "type-case": [2, "always", "lower-case"],
     "type-enum": [
